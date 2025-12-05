1bb2dace93e880f71981503c8a07986a2d421055
Sahra Demir <sahrademir2005@gmail.com>
2025-12-05 13:32:53 +0300

    feat: JWT and Authentication added to the main system with Beste Ulug

--- flight_roster/urls.py
+++ flight_roster/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from passengers.views import health
+from rest_framework_simplejwt.views import ( 
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/health/', health),
     path('api/', include('cabincrew_api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # Login/Token Alma
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Token Yenileme
 ]
