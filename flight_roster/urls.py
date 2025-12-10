67a49cdc93a9b7c8a2d7155446729fbe2bd3fc9d
Bekir Gündağ <bekir.gundagg@gmail.com>
2025-12-10 16:25:35 +0300

    Merged all the apis, but the errors haven’t been fixed yet.

--- flight_roster/urls.py
+++ flight_roster/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.authtoken.views import obtain_auth_token
+
 from passengers.views import health
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
